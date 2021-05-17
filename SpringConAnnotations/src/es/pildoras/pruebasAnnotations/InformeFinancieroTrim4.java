78620f51955521ba286770fb3e2f966ed32ccf11
daniescobar14 <49284609+daniescobar14@users.noreply.github.com>
2021-05-17 15:29:37 +0200

    hasta video 23

---
+++ SpringConAnnotations/src/es/pildoras/pruebasAnnotations/InformeFinancieroTrim4.java
@@ -0,0 +1,14 @@
+package es.pildoras.pruebasAnnotations;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class InformeFinancieroTrim4 implements CreacionInformeFinanciero {
+
+	@Override
+	public String getInformeFinanciero() {
+		// TODO Auto-generated method stub
+		return "Informe financiero del cuarto trimestre";
+	}
+
+}
